d94c41b53bdd10a2f008ee0276710c2c9c25bb53
Michael Oberwasserlechner <michaelowl@gmail.com>
2018-10-28 18:09:57 +0100

    feat(ios): create xcode project add podfile

---
+++ ios/ByteowlsCapacitorOauth2/Source/ByteowlsCapacitorOauth2.h
@@ -0,0 +1,19 @@
+//
+//  ByteowlsCapacitorOauth2.h
+//  ByteowlsCapacitorOauth2
+//
+//  Created by Michael Oberwasserlechner on 28.10.18.
+//  Copyright © 2018 BYTEOWLS Software & Consulting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ByteowlsCapacitorOauth2.
+FOUNDATION_EXPORT double ByteowlsCapacitorOauth2VersionNumber;
+
+//! Project version string for ByteowlsCapacitorOauth2.
+FOUNDATION_EXPORT const unsigned char ByteowlsCapacitorOauth2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ByteowlsCapacitorOauth2/PublicHeader.h>
+
+
---
+++ ios/ByteowlsCapacitorOauth2/Source/ByteowlsCapacitorOauth2.swift
@@ -0,0 +1,124 @@
+import Foundation
+import Capacitor
+import OAuthSwift
+
+typealias JSObject = [String:Any]
+
+@objc(OAuth2ClientPlugin)
+public class OAuth2ClientPlugin: CAPPlugin {
+    
+    let PARAM_APP_ID = "appId";
+    let PARAM_IOS_APP_ID = "ios.appId";
+    let PARAM_IOS_CUSTOM_SCHEME = "ios.customScheme";
+    let PARAM_ACCESS_TOKEN_ENDPOINT = "accessTokenEndpoint";
+    let PARAM_AUTHORIZATION_BASE_URL = "authorizationBaseUrl";
+    let PARAM_SCOPE = "scope";
+    let PARAM_STATE = "state";
+    let PARAM_RESOURCE_URL = "resourceUrl";
+    
+    var oauthSwift: OAuth2Swift?
+
+    @objc func authenticate(_ call: CAPPluginCall) {
+        var appId = getString(call, PARAM_APP_ID)
+        let iosAppId: String? = getString(call, PARAM_IOS_APP_ID)
+        if iosAppId != nil {
+            appId = iosAppId
+        }
+        guard let finalAppId = appId, appId != nil else {
+            call.reject("Option '\(PARAM_APP_ID)' or '\(PARAM_IOS_APP_ID)' is required!")
+            return
+        }
+        guard let baseUrl = getString(call, PARAM_AUTHORIZATION_BASE_URL) else {
+            call.reject("Option '\(PARAM_AUTHORIZATION_BASE_URL)' is required!")
+            return
+        }
+        guard let accessTokenEndpoint = getString(call, PARAM_ACCESS_TOKEN_ENDPOINT) else {
+            call.reject("Option '\(PARAM_ACCESS_TOKEN_ENDPOINT)' is required!")
+            return
+        }
+        guard let customScheme = getString(call, PARAM_IOS_CUSTOM_SCHEME) else {
+            call.reject("Option '\(PARAM_IOS_CUSTOM_SCHEME)' is required!")
+            return
+        }
+        guard let resourceUrl = getString(call, PARAM_RESOURCE_URL) else {
+            call.reject("Option '\(PARAM_RESOURCE_URL)' is required!")
+            return
+        }
+        
+        let oauthSwift = OAuth2Swift(
+            consumerKey: finalAppId,
+            consumerSecret: "",
+            authorizeUrl: baseUrl,
+            accessTokenUrl: accessTokenEndpoint,
+            responseType: "code"
+        )
+        
+        self.oauthSwift = oauthSwift
+        oauthSwift.authorizeURLHandler = SafariURLHandler(viewController: bridge.viewController, oauthSwift: oauthSwift)
+        
+        let defaultState = generateState(withLength: 20)
+        let _ = oauthSwift.authorize(
+            withCallbackURL: customScheme,
+            scope: getString(call, PARAM_SCOPE) ?? "",
+            state: getString(call, PARAM_STATE) ?? defaultState,
+            success: { credential, response, parameters in
+                let _ = oauthSwift.client.get(
+                    resourceUrl,
+                    parameters: parameters,
+                    success: { (response) in
+                        if let jsonObj = try? JSONSerialization.jsonObject(with: response.data, options: []) as! JSObject {
+                            call.success(jsonObj)
+                        }
+                    },
+                    failure: { (error) in
+                        call.reject("Access resource failed with \(error.localizedDescription)");
+                    })
+            },
+            failure: { error in
+                call.reject("Authorization failed with \(error.localizedDescription)");
+            }
+        )
+    }
+    
+    private func getConfigObjectDeepest(_ options: [AnyHashable: Any?]!, key: String) -> [AnyHashable:Any?]? {
+        let parts = key.split(separator: ".")
+        
+        var o = options
+        for (_, k) in parts[0..<parts.count-1].enumerated() {
+            if (o != nil) {
+                o = o?[String(k)] as? [String:Any?] ?? nil
+            }
+        }
+        return o
+    }
+    
+    private func getConfigKey(_ key: String) -> String {
+        let parts = key.split(separator: ".")
+        if parts.last != nil {
+            return String(parts.last!)
+        }
+        return ""
+    }
+    
+    
+    
+    private func getValue(_ call: CAPPluginCall, _ key: String) -> Any? {
+        let k = getConfigKey(key)
+        let o = getConfigObjectDeepest(call.options, key: key)
+        return o?[k] ?? nil
+    }
+    
+    private func getString(_ call: CAPPluginCall, _ key: String) -> String? {
+        let value = getValue(call, key)
+        if value == nil {
+            return nil
+        }
+        return value as? String
+    }
+    
+    // Handle callback url which contains now token information
+    public static func handleRedirectUrl(_ url: URL) {
+        OAuthSwift.handle(url: url)
+    }
+    
+}
