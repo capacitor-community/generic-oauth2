eea76abaaf8f7c5ff9b4d6463df75897bb53fd85
Florian Weber <florian.weber@ueberbit.de>
2021-08-08 21:29:41 +0200

    Use json instead of string in assignResponses on Android (#171)

--- android/src/main/java/com/byteowls/capacitor/oauth2/OAuth2Utils.java
+++ android/src/main/java/com/byteowls/capacitor/oauth2/OAuth2Utils.java
@@ -10,10 +10,10 @@ public abstract class OAuth2Utils {
     public static void assignResponses(JSObject resp, String accessToken, AuthorizationResponse authorizationResponse, TokenResponse accessTokenResponse) {
         // #154
         if (authorizationResponse != null) {
-            resp.put("authorization_response", authorizationResponse.jsonSerializeString());
+            resp.put("authorization_response", authorizationResponse.jsonSerialize());
         }
         if (accessTokenResponse != null) {
-            resp.put("access_token_response", accessTokenResponse.jsonSerializeString());
+            resp.put("access_token_response", accessTokenResponse.jsonSerialize());
         }
         if (accessToken != null) {
             resp.put("access_token", accessToken);
