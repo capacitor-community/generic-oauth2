4a857c0edd4d62326f3654152072bb51e05bfcd3
Jarret Hoving <54537997+JarretHoving97@users.noreply.github.com>
2026-04-02 14:48:15 +0200

    Rename Swift package and targets

--- Package.swift
+++ Package.swift
@@ -7,22 +7,23 @@ let package = Package(
     products: [
         .library(
             name: "CapacitorCommunityGenericOauth2",
-            targets: ["GenericOAuth2Plugin"])
+            targets: ["CapacitorCommunityGenericOauth2"])
     ],
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0")
     ],
     targets: [
         .target(
-            name: "GenericOAuth2Plugin",
+            name: "CapacitorCommunityGenericOauth2",
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
                 .product(name: "Cordova", package: "capacitor-swift-pm")
             ],
             path: "ios/Sources/GenericOAuth2Plugin"),
+
         .testTarget(
             name: "GenericOAuth2PluginTests",
-            dependencies: ["GenericOAuth2Plugin"],
+            dependencies: ["CapacitorCommunityGenericOauth2"],
             path: "ios/Tests/GenericOAuth2PluginTests")
     ]
 )
