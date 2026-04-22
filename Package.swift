f5d5cc292e1e6a0b9d172090a21cdb716016c126
tafelnl <35837839+tafelnl@users.noreply.github.com>
2026-04-22 10:46:02 +0200

    ios 14

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "CapacitorCommunityGenericOauth2",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v14)],
     products: [
         .library(
             name: "CapacitorCommunityGenericOauth2",
