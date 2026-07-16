c80eef045e00d294a707888b817ede248432021c
tafelnl <35837839+tafelnl@users.noreply.github.com>
2026-07-16 07:37:02 +0200

    chore: clean start

--- android/src/main/java/com/getcapacitor/community/genericoauth2/handler/AccessTokenCallback.java
+++
@@ -1,9 +0,0 @@
-package com.getcapacitor.community.genericoauth2.handler;
-
-public interface AccessTokenCallback {
-    void onSuccess(String accessToken);
-
-    void onCancel();
-
-    void onError(Exception error);
-}
--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  verbose: true,
-  testEnvironment: 'node',
-  globals: {
-    window: {},
-  },
-};
