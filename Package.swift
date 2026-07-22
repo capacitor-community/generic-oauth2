672d543ba45fd4b761b39d410f63f31ef2e33dea
tafelnl <35837839+tafelnl@users.noreply.github.com>
2026-07-22 10:29:51 +0200

    feat!: run `npx @capacitor/plugin-migration-v7-to-v8@latest`

--- Package.swift
+++ Package.swift
@@ -3,14 +3,14 @@ import PackageDescription
 
 let package = Package(
     name: "CapacitorCommunityGenericOauth2",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "CapacitorCommunityGenericOauth2",
             targets: ["CapacitorCommunityGenericOauth2"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0"),
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0"),
         .package(url: "https://github.com/OAuthSwift/OAuthSwift.git", from: "2.2.0")
     ],
     targets: [
